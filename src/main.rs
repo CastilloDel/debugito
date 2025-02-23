46d4c68260112a7145154bc46975dfc8d7a6894f
CastilloDel <delcastillodelarosadaniel@gmail.com>
2025-02-23 13:45:11 +0100

    Add breakpoints and refactor everything

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,92 @@ use nix::{
         ptrace::{getregs, step, traceme},
         wait::wait,
     },
-    unistd::{ForkResult, execv, fork},
+    unistd::{ForkResult, Pid, execv, fork},
 };
-use std::{env, ffi::CString, path::Path, thread::sleep, time::Duration};
+use std::{
+    env,
+    ffi::CString,
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
 fn main() {
     let exec_path = Path::new(&env::args().nth(1).unwrap())
         .canonicalize()
         .unwrap();
+    let breakpoint = env::args().nth(2).unwrap().parse::<Breakpoint>().unwrap();
     let loader = addr2line::Loader::new(exec_path.clone()).unwrap();
+    let pid = launch_fork(&exec_path);
+    let status = wait().unwrap();
+    if let nix::sys::wait::WaitStatus::Exited(_, _) = status {
+        panic!("Child exited")
+    }
+    let proc_map = get_range_for_program_source_code(pid.as_raw() as u64, &exec_path);
+    continue_until_breakpoint(pid, breakpoint, proc_map, loader);
+    println!("Here we are");
+}
+
+fn launch_fork(executable: &Path) -> Pid {
     match unsafe { fork() }.unwrap() {
         ForkResult::Child => {
             traceme().expect("I don't want to be traced");
-            execv(&CString::new(exec_path.to_str().unwrap()).unwrap(), &[c""]).unwrap();
+            execv(&CString::new(executable.to_str().unwrap()).unwrap(), &[c""]).unwrap();
+            unreachable!()
         }
-        ForkResult::Parent { child: pid } => loop {
-            loop {
-                let status = wait().unwrap();
-                match status {
-                    nix::sys::wait::WaitStatus::Exited(_, _) => panic!("Child exited"),
-                    _ => {}
-                }
-                let proc_map = get_range_for_program_source_code(pid.as_raw() as u64, &exec_path);
-                let registers = getregs(pid).unwrap();
+        ForkResult::Parent { child: pid } => return pid,
+    }
+}
 
-                if registers.rip > proc_map.address_range.begin
-                    && registers.rip < proc_map.address_range.end
+fn continue_until_breakpoint(
+    pid: Pid,
+    breakpoint: Breakpoint,
+    proc_map: rsprocmaps::Map,
+    loader: addr2line::Loader,
+) {
+    loop {
+        do_step(pid);
+        let registers = getregs(pid).unwrap();
+        if registers.rip < proc_map.address_range.begin
+            || registers.rip >= proc_map.address_range.end
+        {
+            continue;
+        }
+        let address_in_file = registers.rip - proc_map.address_range.begin + proc_map.offset;
+        if let Ok(Some(location)) = loader.find_location(address_in_file) {
+            if let (Some(line), Some(file)) = (location.line, location.file) {
+                if file == breakpoint.file.to_str().unwrap()
+                    && line as usize == breakpoint.line_number
                 {
-                    match loader.find_location(
-                        registers.rip - proc_map.address_range.begin + proc_map.offset,
-                    ) {
-                        Ok(Some(location)) => {
-                            match (location.line, location.file) {
-                                (Some(line), Some(file)) => {
-                                    if file.contains("example") {
-                                        println!("{}", exec_path.to_str().unwrap());
-                                        println!("line:{} {:?}", file, line);
-                                        sleep(Duration::from_secs(1));
-                                    }
-                                }
-                                _ => {}
-                            };
-                        }
-                        _ => {}
-                    }
+                    println!("line:{} {:?}", file, line);
+                    return;
                 }
-                step(pid, None).unwrap();
-            }
-        },
+            };
+        }
+    }
+}
+
+fn do_step(pid: Pid) {
+    step(pid, None).unwrap();
+    let status = wait().unwrap();
+    if let nix::sys::wait::WaitStatus::Exited(_, _) = status {
+        panic!("Child exited")
+    }
+}
+
+struct Breakpoint {
+    file: PathBuf,
+    line_number: usize,
+}
+
+impl FromStr for Breakpoint {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (file, number) = s.split_once(":").unwrap();
+        Ok(Self {
+            file: Path::new(file).canonicalize().unwrap(),
+            line_number: number.parse().unwrap(),
+        })
     }
 }
 
